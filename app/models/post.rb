0e81fa67ec1f45d79d5df34efafa01f86f0857cf
C0derM0hamed <mohamedmostafa2011360@gmail.com>
2026-06-15 01:13:45 +0300

    Implement API endpoints and add collection export

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-  belongs_to :creator
+  belongs_to :creator,
+             class_name: "User"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+Post.destroy_all
+
+user = User.create!(
+  full_name: "Mohamed Mostafa",
+  email: "mohamed@example.com"
+)
+
+Post.create!(
+  title: "First API Post",
+  body: "Testing Rails API",
+  creator: user
+)
